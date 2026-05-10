d5478b1c8ecab500c0183754d2dd11b0383286be
okamuratakeru <takeokamura11@gmail.com>
2026-05-10 16:56:37 +0900

    feat:uuidを追加

--- models/memo.rb
+++ models/memo.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'securerandom'
 
 # メモデータのCRUD操作を担当するモデル
 class Memo
@@ -13,27 +14,26 @@ class Memo
 
   # IDで1件取得
   def self.find(id)
-    all.find { |memo| memo['id'] == id.to_i }
+    all.find { |memo| memo['id'] == id }
   end
 
   # メモを追加
   def self.create(title, description)
     memos = all
-    new_id = memos.empty? ? 1 : memos.last['id'] + 1
-    memos << { 'id' => new_id, 'title' => title, 'description' => description }
+    memos << { 'id' => SecureRandom.uuid, 'title' => title, 'description' => description }
     save(memos)
   end
 
   # IDのメモを削除
   def self.destroy(id)
-    memos = all.reject { |memo| memo['id'] == id.to_i }
+    memos = all.reject { |memo| memo['id'] == id }
     save(memos)
   end
 
   # IDのメモを更新
   def self.update(id, title, description)
     memos = all
-    memo = memos.find { |m| m['id'] == id.to_i }
+    memo = memos.find { |m| m['id'] == id }
     memo['title'] = title
     memo['description'] = description
     save(memos)
